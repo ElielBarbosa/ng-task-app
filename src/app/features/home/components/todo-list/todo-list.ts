2b9db2d0829ab115e191f2fc2dcefe755c220ebc
Eliel Barbosa <elielbarbosads@gmail.com>
2025-09-11 22:40:24 -0300

    feat: implementar componente todo-list

--- src/app/features/home/components/todo-list/todo-list.ts
+++ src/app/features/home/components/todo-list/todo-list.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { TodoButtonDeleteAll } from '../todo-button-delete-all/todo-button-delete-all';
+import { TodoInputAddAll } from '../todo-input-add-all/todo-input-add-all';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [TodoButtonDeleteAll, TodoInputAddAll],
   templateUrl: './todo-list.html',
-  styleUrl: './todo-list.scss'
+  styleUrl: './todo-list.scss',
 })
-export class TodoList {
-
-}
+export class TodoList {}
