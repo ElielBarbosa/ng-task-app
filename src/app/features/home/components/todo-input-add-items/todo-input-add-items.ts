ea3932d75e69aeeb730b19b56e6286fd1747f417
Eliel Barbosa <elielbarbosads@gmail.com>
2025-09-20 16:24:44 -0300

    feat: adicionar lógica aos componentes

--- src/app/features/home/components/todo-input-add-items/todo-input-add-items.ts
+++ src/app/features/home/components/todo-input-add-items/todo-input-add-items.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-input-add-items',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './todo-input-add-items.html',
   styleUrl: './todo-input-add-items.scss',
 })
-export class TodoInputAddItems {}
+export class TodoInputAddItems {
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = '';
+
+  public submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = '';
+    }
+  }
+
+  constructor() {}
+}
--- src/app/features/home/components/todo-list/todo-list.ts
+++ src/app/features/home/components/todo-list/todo-list.ts
@@ -2,10 +2,32 @@ import { Component } from '@angular/core';
 import { TodoButtonDeleteAll } from '../todo-button-delete-all/todo-button-delete-all';
 import { TodoInputAddItems } from '../todo-input-add-items/todo-input-add-items';
 
+//interface
+import { TaskList } from '../model/task-list';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-todo-list',
-  imports: [TodoButtonDeleteAll, TodoInputAddItems],
+  imports: [TodoButtonDeleteAll, CommonModule, TodoInputAddItems, FormsModule, JsonPipe],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.scss',
 })
-export class TodoList {}
+export class TodoList {
+  public taskList: Array<TaskList> = [];
+
+  constructor() {}
+
+  public deleteItemTaskList(indexEvent: number) {
+    this.taskList.splice(indexEvent, 1);
+  }
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja ralmente Deletar tudo?');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
+  public setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false });
+  }
+}
